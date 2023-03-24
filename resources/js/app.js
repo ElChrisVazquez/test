d44b441057b225730a37f4213c0f36f30b55087e
ElChrisVazquez <thechrisvazquez@gmail.com>
2023-03-24 12:55:38 -0600

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,4 @@
+require('./bootstrap');
+import { Chart } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+Chart.register(ChartDataLabels);
---
+++ resources/views/home.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div style="width: 400px;"><canvas id="pie-chart"></canvas></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('scripts')
+    <script>
+        var data = [{
+            data: [50, 50],
+            backgroundColor: [
+                "#4b77a9",
+                "#5f255f",
+                "#d21243",
+                "#B27200"
+            ],
+            borderColor: "#fff"
+        }];
+
+        var options = {
+            tooltips: {
+                enabled: false
+            },
+            plugins: {
+                datalabels: {
+                    formatter: function(value, ctx) {
+                        return value + '%';
+                    },
+                    color: '#fff',
+                }
+            }
+        };
+
+        var ctx = document.getElementById("pie-chart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'pie',
+            data: {
+                labels: ['India', 'China', 'US', 'Canada'],
+                datasets: data
+            },
+            options: options,
+            plugins: [ChartDataLabels],
+        });
+
+        // const ctx = document.getElementById('myChart');
+        // const myChart = new Chart(ctx, {
+        //     type: 'bar',
+        //     data: {
+        //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        //         datasets: [{
+        //             label: '# of Votes',
+        //             data: [12, 19, 3, 5, 2, 3],
+        //             backgroundColor: [
+        //                 'rgba(255, 99, 132, 0.2)',
+        //                 'rgba(54, 162, 235, 0.2)',
+        //                 'rgba(255, 206, 86, 0.2)',
+        //                 'rgba(75, 192, 192, 0.2)',
+        //                 'rgba(153, 102, 255, 0.2)',
+        //                 'rgba(255, 159, 64, 0.2)'
+        //             ],
+        //             borderColor: [
+        //                 'rgba(255, 99, 132, 1)',
+        //                 'rgba(54, 162, 235, 1)',
+        //                 'rgba(255, 206, 86, 1)',
+        //                 'rgba(75, 192, 192, 1)',
+        //                 'rgba(153, 102, 255, 1)',
+        //                 'rgba(255, 159, 64, 1)'
+        //             ],
+        //             borderWidth: 1
+        //         }]
+        //     },
+        //     options: {
+        //         scales: {
+        //             y: {
+        //                 beginAtZero: true
+        //             }
+        //         }
+        //     }
+        // });
+    </script>
+@endsection
